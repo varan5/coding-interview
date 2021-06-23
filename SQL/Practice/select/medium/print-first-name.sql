bea53ef08be3fabe9ca371e02285d4ae9687a768
varan5 <varan2.bhalla@gmail.com>
2021-06-24 02:34:05 +0530

    print-first-name.sql added

---
+++ SQL/Practice/select/medium/print-first-name.sql
@@ -0,0 +1,5 @@
+Write an SQL query to print details for Workers with the first name as “Vipul” and “Satish” from Worker table.
+ 
+ 
+ 
+ SELECT * FROM Worker WHERE FIRST_NAME IN ('Vipul', 'Satish');
