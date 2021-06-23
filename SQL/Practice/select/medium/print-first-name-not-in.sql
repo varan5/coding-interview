d3eae841356360e697c9fb0f8baea7b4f441e4fd
varan5 <varan2.bhalla@gmail.com>
2021-06-24 02:36:47 +0530

    print-first-name-not-in.sql added

---
+++ SQL/Practice/select/medium/print-first-name-not-in.sql
@@ -0,0 +1,6 @@
+Write an SQL query to print details of workers excluding first names, “Vipul” and “Satish” from Worker table.
+
+
+
+
+SELECT * FROM Worker WHERE FIRST_NAME NOT IN ('Vipul', 'Satish');
