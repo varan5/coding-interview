6915a3f38e01c7434b6a8addc4118da1e2f2a243
varan5 <varan2.bhalla@gmail.com>
2021-06-24 03:44:50 +0530

    DSA reverse-string.java added

---
+++ DSA/Practice/string/easy/reverse-string.java
@@ -0,0 +1,23 @@
+package com.company;
+
+// Algorithmic Complexities
+// Time:  O(n) - Where 'n' is the length of the string
+// Space: O(1) - Since, we are using variables of constant space
+
+public class Coding {
+    public char[] reverseString(char[] string) {
+        int startIndex = 0;
+        int endIndex = string.length -1;
+        char temporaryCharacter;
+        while (startIndex < endIndex) {
+            temporaryCharacter = string[startIndex];
+            string[startIndex] = string[endIndex];
+            string[endIndex] = temporaryCharacter;
+            
+            startIndex ++;
+            endIndex --;
+        }
+        
+        return string;
+    }
+}
