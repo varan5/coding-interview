b73ba288f585de652d44abacb3250afb6c9753fd
varan5 <varan2.bhalla@gmail.com>
2021-06-24 02:52:10 +0530

    create-table-persons.sql added

---
+++ SQL/Practice/create/easy/create-table-persons.sql
@@ -0,0 +1,11 @@
+The following SQL creates a table called "Persons" that contains five columns: PersonID, LastName, FirstName, Address, and City:
+
+
+
+CREATE TABLE Persons (
+	PersonID INT, 
+	LastName VARCHAR(255),
+	FirstName VARCHAR(255),
+	Address VARCHAR(255),
+	City VARCHAR(255)
+);
