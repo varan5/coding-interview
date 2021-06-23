d57871ae0ec86350076d2f20fc1f427a74085085
varan5 <varan2.bhalla@gmail.com>
2021-06-24 02:57:13 +0530

    create-table-employee.sql added

---
+++ SQL/Practice/create/easy/create-table-employee.sql
@@ -0,0 +1,12 @@
+Create a table named Employee with basic attributes.
+
+
+
+CREATE TABLE Employee (
+	EmployeeID INT PRIMARY KEY,
+	FirstName VARCHAR(255),
+	LastName VARCHAR(255),
+	Email VARCHAR(255),
+	Address VARCHAR(255),
+	City VARCHAR(255)
+);
