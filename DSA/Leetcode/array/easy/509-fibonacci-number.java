0b988a50d6120b9942f175d64a18aec9e205ee6a
varan5 <67020950+varan5@users.noreply.github.com>
2021-07-04 23:59:24 +0530

    DSA 509-fibonacci-number.java added

---
+++ DSA/Leetcode/array/easy/509-fibonacci-number.java
@@ -0,0 +1,25 @@
+
+// Algorithmic Complexities
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+    public int fib(int number) {
+        if (number == 0)
+            return 0;
+        if (number == 1)
+            return 1;
+        
+        int firstNumber = 0;
+        int secondNumber = 1;
+        int sum = 0;
+        while (number > 1) {
+            sum = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = sum;
+            
+            number --;
+        }
+        return sum;
+    }
+}
