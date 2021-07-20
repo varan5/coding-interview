170afc6f618afdcc53d4c50f60f0d068126a8ae3
varan5 <67020950+varan5@users.noreply.github.com>
2021-07-20 22:41:51 +0530

    Create contains-duplicate.java

---
+++ DSA/Practice/hashset/easy/contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int element: nums) {
+            if (set.contains(element)) {
+                return true;
+            }
+            else {
+                set.add(element);
+            }
+        }
+        
+        return false;
+    }
+}
