7edfe76f89615ab891c2b7c0605ba5ab7f75f30a
varan5 <67020950+varan5@users.noreply.github.com>
2021-07-27 23:07:01 +0530

    Update middle-of-the-linked-list.java

--- DSA/Practice/linked-list/easy/middle-of-the-linked-list.java
+++ DSA/Practice/linked-list/easy/middle-of-the-linked-list.java
@@ -12,7 +12,6 @@ class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode slowPointer = head;
         ListNode fastPointer = head;
-        
         while (fastPointer != null && fastPointer.next != null) {
             slowPointer = slowPointer.next;
             fastPointer = fastPointer.next.next;
